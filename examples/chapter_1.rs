7998841785e4cc77f665df466cf1fda6021fa091
Alexey <alexvoit70@gmail.com>
2025-01-18 22:28:33 +0700

    add chapter_1 example

---
+++ examples/chapter_1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    const THREE_AND_A_BIT: f32 = 3.4028236;
+    const THREE_AND_A_BIT_F64: f64 = 3.4028236;
+    println!("{}", THREE_AND_A_BIT);
+    println!("{}", THREE_AND_A_BIT_F64);
+}
